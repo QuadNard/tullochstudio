6a75eb47ce8d399a81f8a1c36a1eb891cfb09a1f
QuadNard <lizardkingnardo@gmail.com>
2023-10-15 20:04:35 -0400

    layout change

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <section className="text-default-color-gray min-h-screen">
-      <div className="bg-[url('/noise.png')] overflow-x-hidden inset-0 mix-blend-hard-light pointer dark:hidden">
+      <div className="bg-[url('/noise.png')] overflow-x-hidden inset-0 md:mix-blend-hard-light pointer dark:hidden">
         {children}
       </div>
     </section>
